3bc77d7309bb958b71de05975e96cc4b1452a541
Elwynn Neglokpe <elwynn.neglokpe@gmail.com>
2020-05-19 19:28:30 +0200

    Create agent on a non-continuous grid

--- src/model/Constants.java
+++ src/model/Constants.java
@@ -1,17 +1,14 @@
 package model;
 
 public final class Constants 
-{
-	
-	public static String MAIN_PROPERTIES_FILE = "main.properties";
-	public static String SECONDARY_PROPERTIES_FILE = "secondary.properties";
-	
+{	
 	public static String SIM = "Simulation";
 	public static String ANA = "Analyse";
 	public static String ENV = "Environnement";
 	
 	public static int FRAME_SIZE = 400;
-	public static int GRID_SIZE = 300;
+	public static int GRID_SIZE = 30;
+	public static int NUM_AGENT = 10;
 }
 
 		
--- src/model/Model.java
+++ src/model/Model.java
@@ -1,7 +1,9 @@
 package model;
 
+import agents.*;
 import sim.engine.SimState;
 import sim.field.grid.SparseGrid2D;
+import sim.util.Int2D;
 
 public class Model extends SimState 
 {
@@ -16,5 +18,23 @@ public class Model extends SimState
 	public void start() 
 	{
 		super.start();
+		yard.clear();
+		addRandomAgents();
+	}
+	
+	private void addRandomAgents() { 
+		for(int  i  =  0;  i  <  Constants.NUM_AGENT;  i++) {
+			Int2D position = randomPosition();
+			double angle = this.random.nextInt(360);
+			AgentType e = new AgentType(position.x, position.y, 1, 1, angle);
+			yard.setObjectLocation(e, position);
+			schedule.scheduleRepeating(e);
+		}
+	}
+	
+	public Int2D randomPosition() {
+		int x = this.random.nextInt(Constants.GRID_SIZE);
+		int y = this.random.nextInt(Constants.GRID_SIZE);
+		return new Int2D(x, y);
 	}
 }
