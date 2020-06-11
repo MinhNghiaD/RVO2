50fa12e6520803f6430b78239667a60da4e9fa8f
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-06-11 13:25:59 +0200

    construct KD-Tree for obstacles

--- src/kdtree/KDTreeObstacle.java
+++ src/kdtree/KDTreeObstacle.java
@@ -1,20 +1,51 @@
 package kdtree;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.Vector;
 
 import clearpath.Obstacle;
 import clearpath.ObstacleVertex;
-import clearpath.RVO;
 
 public class KDTreeObstacle
 {
-    KDTreeObstacle()
+    public KDTreeObstacle(List<Obstacle> obstacles)
     {
+        this.obstacles = obstacles;
         
+        List<ObstacleVertex> vertices = new ArrayList<ObstacleVertex>();
+        
+        for (Obstacle obstacle: obstacles)
+        {
+            vertices.addAll(obstacle.getVertices());
+        }
+        
+        if (vertices.isEmpty())
+        {
+            root = null;
+        }
+        else
+        {
+            root = new KDNodeObstacle(vertices);
+        }
+    }
+    
+    public List<Obstacle> getObstacles()
+    {
+        return obstacles;
+    }
+    
+    public TreeMap<Double, Vector<KDNodeObstacle>> getClosestObstacles(double[] position, double sqRange)
+    {
+        TreeMap<Double, Vector<KDNodeObstacle>> closestObstacle = new TreeMap<Double, Vector<KDNodeObstacle>>();
+        
+        root.getClosestNeighbors(closestObstacle, position, sqRange);
+    
+        return closestObstacle;
     }
     
     
     private KDNodeObstacle root;
-    
     private List<Obstacle> obstacles;
 }
